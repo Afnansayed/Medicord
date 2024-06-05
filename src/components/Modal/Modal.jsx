2afad611f4ea2679059a2107af4e2d821bf4ae09
Afnansayed <afnansayed1973@gmail.com>
2024-06-06 02:08:36 +0600

    ui design  modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,56 @@
 import  PropTypes from 'prop-types'
+import { useForm } from "react-hook-form"
+import { MdCancelPresentation } from "react-icons/md";
+import { FaArrowRightLong  } from "react-icons/fa6";
+
 const Modal = ({ showModal, handleClose, handleJoinCamp,campData}) => {
     if (!showModal) {
         return null;
     }
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
+      const onSubmit = (data) => {
+        console.log(data)
         const participantData = {
-            name: formData.get("name"),
-            email: formData.get("email"),
+            name: data.name,
+            email: data.email,
         };
         handleJoinCamp(participantData);
-    };
+    }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     const formData = new FormData(event.target);
+    //     const participantData = {
+    //         name: formData.get("name"),
+    //         email: formData.get("email"),
+    //     };
+    //     handleJoinCamp(participantData);
+    // };
 
     return (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
-            <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+            <div className="relative top-20 mx-auto p-5 border w-96 md:w-[600px] shadow-lg rounded-md bg-gray-50">
                 <div className="mt-3 text-center">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">Join Camp</h3>
                     <div className="mt-2 px-7 py-3">
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="mb-4">
                                 <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
                                     Name
                                 </label>
                                 <input
+                                    {...register('name')}
                                     id="name"
                                     name="name"
                                     type="text"
                                     required
+                                    disabled
                                     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                     defaultValue={campData?.date}
                                 />
@@ -39,6 +60,7 @@ const Modal = ({ showModal, handleClose, handleJoinCamp,campData}) => {
                                     Email
                                 </label>
                                 <input
+                                    {...register('email',{required:true})}
                                     id="email"
                                     name="email"
                                     type="email"
@@ -49,16 +71,16 @@ const Modal = ({ showModal, handleClose, handleJoinCamp,campData}) => {
                             <div className="flex items-center justify-between">
                                 <button
                                     type="submit"
-                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center gap-6"
                                 >
-                                    Join
+                                    Join <FaArrowRightLong />
                                 </button>
                                 <button
                                     type="button"
                                     onClick={handleClose}
-                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline flex items-center gap-2"
                                 >
-                                    Cancel
+                                    <MdCancelPresentation /> Cancel
                                 </button>
                             </div>
                         </form>
