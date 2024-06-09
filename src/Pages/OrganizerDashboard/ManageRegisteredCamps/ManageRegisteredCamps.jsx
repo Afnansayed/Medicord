99de16f9258bccf8e01fd9aedfbccf8d31c2da32
Afnansayed <afnansayed1973@gmail.com>
2024-06-09 22:09:03 +0600

    registered camp functionality implement

--- src/Pages/OrganizerDashboard/ManageRegisteredCamps/ManageRegisteredCamps.jsx
+++ src/Pages/OrganizerDashboard/ManageRegisteredCamps/ManageRegisteredCamps.jsx
@@ -2,17 +2,47 @@ import { useQuery } from "@tanstack/react-query";
 import UseAxiosSecure from "../../../Hooks/UseAxiosSecure/UseAxiosSecure";
 import { GiConfirmed } from "react-icons/gi";
 import { MdCancel } from "react-icons/md";
+import Swal from "sweetalert2";
 
 
 const ManageRegisteredCamps = () => {
     const axiosSecure = UseAxiosSecure();
-    const {data: registeredCamps = [] , refetch} = useQuery({
-          queryKey: ['registeredCamps'],
-          queryFn: async () => {
+    const { data: registeredCamps = [], refetch } = useQuery({
+        queryKey: ['registeredCamps'],
+        queryFn: async () => {
             const res = await axiosSecure.get('/participantCamps');
             return res.data;
-          }
+        }
     })
+
+    //delete 
+    const handleRegisteredCampDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#181ca3",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //delete camps registered
+                axiosSecure.delete(`/participantCamps/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              });
+                              refetch();
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div className="container mx-auto p-6 mt-12 md:mt-0">
             <div className="overflow-x-auto">
@@ -29,24 +59,24 @@ const ManageRegisteredCamps = () => {
                     </thead>
                     <tbody>
                         {
-                        registeredCamps.map(registerCamp => <tr key={registerCamp?._id}>
-                            <td className="py-2 px-4 border-b">{registerCamp?.participantName}</td>
-                            <td className="py-2 px-4 border-b">{registerCamp?.campName}</td>
-                            <td className="py-2 px-4 border-b">{registerCamp?.campFees} $</td>
-                            <td className="py-2 px-4 border-b">{registerCamp?.paymentStatus} </td>
-                            <td className="py-2 px-4 border-b">
-                                {
-                                    registerCamp?.paymentStatus === 'Unpaid'? <p>{registerCamp?.confirmationStatus}</p> : <p className="px-3 btn btn-sm  bg-[#181ca3] text-[#ffff]">{registerCamp?.confirmationStatus}<GiConfirmed className="text-green-600" /></p> 
-                                }
-                            </td>
-                            <td className="py-2 px-4 border-b">
-                                {
-                                  registerCamp?.confirmationStatus === "Confirmed" && registerCamp?.paymentStatus === 'Paid' ? <p  disabled className="px-3 btn btn-sm text-lg bg-green-600  text-[#ffff]"><GiConfirmed /></p> :<p className="px-3 btn btn-sm text-lg bg-red-600 text-[#ffff]"><MdCancel /></p>
-                                }
-                            </td>
-                        </tr>)
+                            registeredCamps.map(registerCamp => <tr key={registerCamp?._id}>
+                                <td className="py-2 px-4 border-b">{registerCamp?.participantName}</td>
+                                <td className="py-2 px-4 border-b">{registerCamp?.campName}</td>
+                                <td className="py-2 px-4 border-b">{registerCamp?.campFees} $</td>
+                                <td className="py-2 px-4 border-b">{registerCamp?.paymentStatus} </td>
+                                <td className="py-2 px-4 border-b">
+                                    {
+                                        registerCamp?.paymentStatus === 'Unpaid' ? <p>{registerCamp?.confirmationStatus}</p> : <p className="px-3 btn btn-sm  bg-[#181ca3] text-[#ffff]">{registerCamp?.confirmationStatus}</p>
+                                    }
+                                </td>
+                                <td className="py-2 px-4 border-b">
+                                    {
+                                        registerCamp?.confirmationStatus === "Confirmed" && registerCamp?.paymentStatus === 'Paid' ? <p disabled className="px-3 btn btn-sm text-lg bg-green-600  text-[#ffff]"><GiConfirmed /></p> : <p onClick={() => handleRegisteredCampDelete(registerCamp?._id)} className="px-3 btn btn-sm text-lg bg-red-600 text-[#ffff]"><MdCancel /></p>
+                                    }
+                                </td>
+                            </tr>)
                         }
-                        
+
 
                     </tbody>
                 </table>
