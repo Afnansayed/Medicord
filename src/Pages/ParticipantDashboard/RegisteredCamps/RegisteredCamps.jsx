8310ad228b29cc99e1f98b3441d86c857509b46f
Afnansayed <afnansayed1973@gmail.com>
2024-06-10 00:03:39 +0600

    my register delete

--- src/Pages/ParticipantDashboard/RegisteredCamps/RegisteredCamps.jsx
+++ src/Pages/ParticipantDashboard/RegisteredCamps/RegisteredCamps.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../../../Providers/AuthProvider";
 import UseAxiosSecure from "../../../Hooks/UseAxiosSecure/UseAxiosSecure";
 import { GiConfirmed } from "react-icons/gi";
 import { MdCancel } from "react-icons/md";
+import Swal from "sweetalert2";
 
 
 const RegisteredCamps = () => {
@@ -16,6 +17,34 @@ const RegisteredCamps = () => {
              return res.data;
          }
     })
+
+    const handleMyRegisterDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#181ca3",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //delete camps registered
+                axiosSecure.delete(`/participantCamps/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                              Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                              });
+                              refetch();
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div className="container mx-auto p-6 mt-12 md:mt-0">
         <div className="overflow-x-auto">
@@ -28,6 +57,7 @@ const RegisteredCamps = () => {
                         <th className="py-2 px-4 border-b">Payment Status</th>
                         <th className="py-2 px-4 border-b">Confirmation Status</th>
                         <th className="py-2 px-4 border-b">Cancel</th>
+                        <th className="py-2 px-4 border-b">Feedback</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -49,7 +79,12 @@ const RegisteredCamps = () => {
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {
-                                    myRegisterCamp?.confirmationStatus === "Confirmed" && myRegisterCamp?.paymentStatus === 'Paid' ? <p disabled className="px-3 btn btn-sm text-lg bg-green-600  text-[#ffff]"><GiConfirmed /></p> : <p  className="px-3 btn btn-sm text-lg bg-red-600 text-[#ffff]"><MdCancel /></p>
+                                     myRegisterCamp?.paymentStatus === 'Paid' ? <p disabled className="px-3 btn btn-sm text-lg bg-green-600  text-[#ffff]"><GiConfirmed /></p> : <p onClick={() => handleMyRegisterDelete(myRegisterCamp?._id)} className="px-3 btn btn-sm text-lg bg-red-600 text-[#ffff]"><MdCancel /></p>
+                                }
+                            </td>
+                            <td className="py-2 px-4 border-b">
+                                {
+                                     myRegisterCamp?.paymentStatus === 'Paid' ? <p className="px-3 btn btn-sm text-lg bg-[#181ca3]  text-[#ffff]">Feedback</p> : <p  >N/A</p>
                                 }
                             </td>
                         </tr>)
